987e6b18290a1b8c7224a76af81fb9fb7bafa750
Ar3s-bo <smomone15@gmail.com>
2026-05-17 13:48:16 +0200

    fix download dir ubuntu

--- backend/transfer.py
+++ backend/transfer.py
@@ -3,10 +3,12 @@ from pathlib import Path
 import shutil
 import httpx
 import json
+from platformdirs import user_downloads_dir
+
 
 router = APIRouter()
 
-DOWNLOAD_DIR = Path.home() / "Downloads"
+DOWNLOAD_DIR = Path(user_downloads_dir())
 DOWNLOAD_DIR.mkdir(exist_ok=True)
 
 @router.post("/send")
